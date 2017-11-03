ad15c9c9af7f3ec75e4f04e81133c03c697f0813
Jakub Białas <jb@3a.pl>
2017-11-03 14:41:58 +0100

    Fix draging between containers

--- src/services/drake-store.service.ts
+++ src/services/drake-store.service.ts
@@ -143,7 +143,7 @@ export class DrakeStoreService {
               } else {
                 if (el.parentNode !== source) {
                   // add element back, let angular remove it
-                  source.append(el);
+                  source.appendChild(el);
                 }
                 sourceModel.splice(dragIndex, 1);
               }
