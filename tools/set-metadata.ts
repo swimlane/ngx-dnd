3f03b5e71b45a2da894dd771ed8e774d293b17ac
Jayson Harshbarger <hypercubed@gmail.com>
2018-06-18 15:41:37 -0600

    Tools

---
+++ tools/set-metadata.ts
@@ -0,0 +1,41 @@
+/* The following code is from https://github.com/ngxs/store/blob/master/tools/set-metadata.ts */
+
+import { writeFile } from 'fs';
+import { getProjects } from './utils';
+
+async function main() {
+  const ngxsJson = require('../package.json');
+  const keysToCopy = [
+    'version',
+    'repository',
+    'keywords',
+    'author',
+    'contributors',
+    'license',
+    'bugs',
+    'homepage'
+  ];
+
+  const packages = getProjects();
+  for (const pack of packages) {
+    
+    const packPath = `${pack.path}/package.json`;
+    const packPackage = require(packPath);
+
+    // copy all meta data from the root package.json into all packages
+    for (const key of keysToCopy) {
+      packPackage[key] = ngxsJson[key];
+    }
+
+    // save the package file after we have updated the keys and peerDependencies
+    await writeFile(packPath, JSON.stringify(packPackage, null, 2), err => {
+      if (err) {
+        console.error('Write failed!');
+      }
+    });
+  }
+
+  console.log(`package version set to ${ngxsJson.version}`);
+}
+
+main();
---
+++ tools/utils.ts
@@ -0,0 +1,13 @@
+import { exec } from 'child_process';
+import { resolve } from 'path';
+
+export function getProjects(): any[] {
+  const { projects } = require('../angular.json');
+  return Object.keys(projects).map(key => {
+    const path = resolve(__dirname, '../', projects[key].root)
+    return {
+      ...projects[key],
+      path
+    };
+  }).filter(project => project.root !== '');
+}
