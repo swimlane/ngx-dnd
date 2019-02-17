93aeb0ccbf2bb895b4d95c49d4e61faa099a850d
bostondevin <Devin.McQueeney@americantower.com>
2019-02-17 09:30:18 -0700

    Fix lazy loaded module issue
    Fixes the bug #130

--- projects/swimlane/ngx-dnd/src/lib/ngx-dnd.module.ts
+++ projects/swimlane/ngx-dnd/src/lib/ngx-dnd.module.ts
@@ -7,6 +7,7 @@ import { DragHandleDirective } from './directives/ngx-drag-handle.directive';
 import { ContainerComponent } from './components/container/container.component';
 import { ItemComponent } from './components/item/item.component';
 import { DrakeStoreService } from './services/drake-store.service';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
 
 const components = [ContainerComponent, ItemComponent];
 const directives = [DraggableDirective, DroppableDirective, DragHandleDirective];
@@ -14,7 +15,12 @@ const directives = [DraggableDirective, DroppableDirective, DragHandleDirective]
 @NgModule({
   imports: [CommonModule],
   declarations: [...components, ...directives],
-  exports: [...components, ...directives],
-  providers: [DrakeStoreService]
+  exports: [...components, ...directives]
 })
-export class NgxDnDModule {}
+export class NgxDnDModule {
+  static forRoot(): ModuleWithProviders {
+  return {
+    ngModule: NgxDnDModule,
+    providers: [ DrakeStoreService ]
+  };
+}}
