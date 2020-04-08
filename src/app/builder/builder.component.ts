b04c60c0fdcce0357f731ef6e972dd814867babb
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2020-04-08 16:32:13 -0600

    linting

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -4,32 +4,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-builder',
   templateUrl: './builder.component.html',
   styleUrls: ['./builder.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-export class BuilderComponent implements OnInit {
-
+export class BuilderComponent {
   sourceBuilderTools = [
     { name: 'Section', children: [] as any[], inputType: 'section', icon: 'far fa-square', class: 'wide' },
     { name: 'String', inputType: 'string', icon: 'fas fa-language', class: 'half' },
-    { name: 'Number', inputType: 'number', icon: 'fas fa-hashtag', class: 'half' }
+    { name: 'Number', inputType: 'number', icon: 'fas fa-hashtag', class: 'half' },
   ];
   targetBuilderTools: any[] = [];
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   droppableItemClass = (item: any) => `${item.class} ${item.inputType}`;
 
   builderDrag(e: any) {
     const item = e.value;
-    item.data =
-      item.inputType === 'number'
-        ? (Math.random() * 100) | 0
-        : Math.random()
-            .toString(36)
-            .substring(20);
+    item.data = item.inputType === 'number' ? (Math.random() * 100) | 0 : Math.random().toString(36).substring(20);
   }
 
   log(e: any) {
@@ -39,5 +28,4 @@ export class BuilderComponent implements OnInit {
   canMove(e: any): boolean {
     return e.indexOf('Disabled') === -1;
   }
-
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,8 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+/* IE9, IE10 and IE11 requires all of the following polyfills. */
+
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
 // import 'core-js/es6/function';
@@ -46,17 +47,14 @@ import '@webcomponents/custom-elements/src/native-shim';
 // Used for browsers without a native support of Custom Elements
 import '@webcomponents/custom-elements/custom-elements.min';
 
-
-/** Evergreen browsers require these. **/
+/* Evergreen browsers require these. */
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 
-
-
-/**
+/*
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- **/
+ */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
@@ -64,11 +62,11 @@ import '@webcomponents/custom-elements/custom-elements.min';
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  */
 
- // (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- // (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- // (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+// (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+// (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+// (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
 
- /*
+/*
  * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
  * with the following flag, it will bypass `zone.js` patch for IE/Edge
  */
@@ -77,9 +75,7 @@ import '@webcomponents/custom-elements/custom-elements.min';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
