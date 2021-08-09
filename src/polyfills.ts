36b9cbbdd9f36407d31b87985b93618bb0a1b526
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2021-08-09 12:54:42 -0600

    ng update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -75,7 +75,7 @@ import '@webcomponents/custom-elements/custom-elements.min';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
