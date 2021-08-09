8d2eea69f3135c51793b5a3512b7a01c768b1e0e
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2021-08-09 13:01:05 -0600

    bump cypress and convert to TS

--- cypress/integration/ngx-dnd_spec.ts
+++ cypress/integration/ngx-dnd_spec.ts
@@ -1,7 +1,9 @@
 describe('Demo', () => {
   const dragAndDrop = (fromElement, toElement, location = 'bottomRight') => {
     cy.get(fromElement).first().trigger('mousedown', { which: 1 });
-    cy.get(toElement).trigger('mousemove', location).trigger('mouseup');
+    cy.get(toElement)
+      .trigger('mousemove', (location as any) as Cypress.TriggerOptions)
+      .trigger('mouseup');
   };
 
   const matchOrder = order => {
