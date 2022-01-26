eefa5d5c957597fdd1a0b0c8408d46cc0b37d4cd
Ian Schultz <2207263+ischultz@users.noreply.github.com>
2022-01-26 00:06:22 -0700

    Revert "Convert from TSLint to ESLint"
    This reverts commit d685508d33a6ecd209c39e460fdf03255f7706a8.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ const pkg = require('../../projects/swimlane/ngx-dnd/package.json');
 
 export const environment = {
   production: true,
-  appVersion: pkg.version
+  APP_VERSION: pkg.version
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ const pkg = require('../../projects/swimlane/ngx-dnd/package.json');
 
 export const environment = {
   production: false,
-  appVersion: pkg.version
+  APP_VERSION: pkg.version
 };
 
 /*
